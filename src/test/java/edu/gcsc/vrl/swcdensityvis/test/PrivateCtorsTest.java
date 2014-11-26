479ea918e40f0991ebc60f975fd2d5129ccb2460
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2014-11-26 16:17:33 +0100

    Testing private ctors.

---
+++ src/test/java/edu/gcsc/vrl/swcdensityvis/test/PrivateCtorsTest.java
@@ -0,0 +1,77 @@
+package edu.gcsc.vrl.swcdensityvis.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import edu.gcsc.vrl.swcdensityvis.CuboidUtility;
+import edu.gcsc.vrl.swcdensityvis.DensityUtil;
+import edu.gcsc.vrl.swcdensityvis.EdgeUtility;
+import edu.gcsc.vrl.swcdensityvis.SWCUtility;
+import edu.gcsc.vrl.swcdensityvis.SwappablePairUtility;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author stephan
+ */
+public class PrivateCtorsTest {
+	
+	public PrivateCtorsTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	
+	@Test
+public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+  Constructor<CuboidUtility> constructor = CuboidUtility.class.getDeclaredConstructor();
+  assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+  constructor.setAccessible(true);
+  constructor.newInstance();
+  
+   Constructor<SWCUtility> constructor2 = SWCUtility.class.getDeclaredConstructor();
+  assertTrue(Modifier.isPrivate(constructor2.getModifiers()));
+  constructor2.setAccessible(true);
+  constructor2.newInstance();
+  
+   Constructor<SwappablePairUtility> constructor3 = SwappablePairUtility.class.getDeclaredConstructor();
+  assertTrue(Modifier.isPrivate(constructor3.getModifiers()));
+  constructor3.setAccessible(true);
+  constructor3.newInstance();
+  
+   Constructor<EdgeUtility> constructor4 = EdgeUtility.class.getDeclaredConstructor();
+  assertTrue(Modifier.isPrivate(constructor4.getModifiers()));
+  constructor4.setAccessible(true);
+  constructor4.newInstance();
+  
+
+   Constructor<DensityUtil> constructor5 = DensityUtil.class.getDeclaredConstructor();
+  assertTrue(Modifier.isPrivate(constructor5.getModifiers()));
+  constructor5.setAccessible(true);
+  constructor5.newInstance();
+	}
+}
