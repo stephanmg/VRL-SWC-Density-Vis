867cc6004875d23cf45ae37e3558d5f1237dda67
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2014-12-16 17:03:25 +0100

    Fix for visualization (done in Density-Vis plugin), added new template.

--- src/main/java/edu/gcsc/vrl/swcdensityvis/ComputeSWCDensity.java
+++ src/main/java/edu/gcsc/vrl/swcdensityvis/ComputeSWCDensity.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import javax.vecmath.Vector3f;
 
 /**
  * @brief computes the density
@@ -41,11 +40,7 @@ public class ComputeSWCDensity implements java.io.Serializable {
 		@ParamGroupInfo(group = "Common options|true|Compute the density for the image (stack); Dimensions|true|Dimensions")
 		@ParamInfo(name = "Depth", typeName = "Depth of sampling cube", style = "slider", options = "min=1;max=100") int depth,
 		@ParamGroupInfo(group = "Advanced options|true|Compute the density for the image (stack); Compartment|true|Compartment")
-		@ParamInfo(name = "Type", typeName = "Compartment", style = "selection", options = "value=[\"all\", \"undefined\", \"axon\", \"(basal) dendrite\", \"apical dendrite\", \"fork point\", \"end point\", \"custom\"]") String choice,
-		@ParamGroupInfo(group = "Advanced options|false|Compute the density for the image (stack); Scaling|false|Scaling")
-		@ParamInfo(name = "Custom scaling mode", typeName = "Enable custom scaling", options = "value=false", style = "default") boolean bCustomScale,
-		@ParamGroupInfo(group = "Advanced options|false|Compute the density for the image (stack); Scaling|false|Scaling")
-		@ParamInfo(name = "Factor", typeName = "Custom scaling factor", options = "value=1.0") double custom_scaling_factor
+		@ParamInfo(name = "Type", typeName = "Compartment", style = "selection", options = "value=[\"all\", \"undefined\", \"axon\", \"(basal) dendrite\", \"apical dendrite\", \"fork point\", \"end point\", \"custom\"]") String choice
 	) {
 		HashMap<String, ArrayList<SWCCompartmentInformation>> cells = new HashMap<String, ArrayList<SWCCompartmentInformation>>();
 		try {
@@ -66,27 +61,11 @@ public class ComputeSWCDensity implements java.io.Serializable {
 			eu.mihosoft.vrl.system.VMessage.exception("File not found", e.toString());
 		}
 
-		/// geometry must fit front and back plane (0.1 and 100 is the limiting interval)
-		Vector3f dim = SWCUtility.getDimensions(cells);
-		double backplane = 100.0;
-		double frontplane = 0.1;
-		double scaling_factor = (backplane - frontplane) / (Collections.max(Arrays.asList(dim.x, dim.y, dim.z)));
-		/// auto scale mode
-		if (!bCustomScale) {
-			SWCUtility.scaleAndTransformGeometry(cells, new Vector3f(0.1f, 0.1f, 0.1f), new Vector3f(100.0f - 0.1f, 100.0f - 0.1f, 100.0f - 0.1f));
-			System.err.println("scaling_factor: " + scaling_factor);
-		} else {
-			SWCUtility.scaleAndTransformGeometry(cells, new Vector3f(0.1f, 0.1f, 0.1f), new Vector3f(100.0f - 0.1f, 100.0f - 0.1f, 100.0f - 0.1f), (float) custom_scaling_factor);
-			System.err.println("custom_scaling_factor: " + scaling_factor);
-		}
-		/// least coordinates
-		System.err.println("least coordinates: " + SWCUtility.getBoundingBox(cells).getSecond());
-
 		/// density must respect new rescaled geometry and therefore fit in cuboid
 		Density density = DensityUtil.computeDensity(cells, width, height, depth, choice);
-		Vector3f new_dim = SWCUtility.getDimensions(cells);
-		VTriangleArray vta = new Cube(2 * new_dim.x, 2 * new_dim.y, 2 * new_dim.z).toCSG().toVTriangleArray();
-
+		double dim = Collections.max(Arrays.asList(SWCUtility.getDimensions(cells).x, SWCUtility.getDimensions(cells).y, SWCUtility.getDimensions(cells).z));
+		VTriangleArray vta = new Cube(dim, dim, dim).toCSG().toVTriangleArray();
+		
 		/// return density
 		return new DensityResult(density, vta);
 	}
--- src/main/java/edu/gcsc/vrl/swcdensityvis/DensityImpl.java
+++ src/main/java/edu/gcsc/vrl/swcdensityvis/DensityImpl.java
@@ -8,6 +8,8 @@ import edu.gcsc.vrl.densityvis.VoxelImpl;
 import edu.gcsc.vrl.densityvis.WritableVoxel;
 import eu.mihosoft.vrl.reflection.Pair;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import javax.vecmath.Vector3f;
@@ -17,67 +19,83 @@ import javax.vecmath.Vector3f;
  * @author stephan
  */
 final class DensityImpl implements Density { /// this can get an instance of teh ImportGeometryFooImpl in a general way... => plug in here SWC or Foo or Bar or other Importer
-    /// the SWC "stack"
-    private final HashMap<String, ArrayList<SWCCompartmentInformation>> stack;
-    private final int voxelWidth;
-    private final int voxelHeight;
-    private final int voxelDepth;
-    private final String choice;
-    
-    /// the output voxels
-    private final ArrayList<WritableVoxel> voxels = new ArrayList<WritableVoxel>();
+	/// the SWC "stack"
 
-    /**
-     * @brief computes the average density in each voxel subset
-     * @param stack
-     * @param width
-     * @param height
-     * @param depth 
-     * @param choice
-     */
-    public DensityImpl(HashMap<String, ArrayList<SWCCompartmentInformation>> stack, int voxelWidth, int voxelHeight, int voxelDepth, String choice) {
-	this.stack = stack;
-        this.voxelWidth = voxelWidth;
-        this.voxelHeight = voxelHeight;
-        this.voxelDepth = voxelDepth;
-	this.choice = choice;
-	compute();
-    }
+	private final HashMap<String, ArrayList<SWCCompartmentInformation>> stack;
+	private final int voxelWidth;
+	private final int voxelHeight;
+	private final int voxelDepth;
+	private final String choice;
 
-    /**
-     * Computes the average density for each voxel subset.
-     */
-    private void compute() {
-	/// get the bounding box in physiological units, i. e. µm
-	Pair<Vector3f, Vector3f> bounding = SWCUtility.getBoundingBox(stack);
-	/// compute the density
-	HashMap<Integer, Float> density = SWCUtility.computeDensity(stack, voxelWidth, voxelHeight, voxelDepth, choice); 
+	/// the output voxels
+	private final ArrayList<WritableVoxel> voxels = new ArrayList<WritableVoxel>();
+
+	/// front and back planes
+	private final float backplane = 100.0f;
+	private final float frontplane = 0.1f;
+
 	/**
-	 * @todo computeDensity must respect the voxel width depth and height
+	 * @brief computes the average density in each voxel subset
+	 * @param stack
+	 * @param width
+	 * @param height
+	 * @param depth
+	 * @param choice
 	 */
-	
-	int index = 0;
-	for (float x = bounding.getSecond().x; x < bounding.getFirst().x; x+=this.voxelWidth) {
-		for (float y = bounding.getSecond().y; y < bounding.getFirst().y; y+=this.voxelHeight) {
-		 	  for (float z = bounding.getSecond().z; z < bounding.getFirst().z; z+=this.voxelDepth) {
-				  if (density.containsKey(index)) {
-			   	voxels.add(new VoxelImpl((int)x, (int)y, (int)z, this.voxelWidth, this.voxelHeight, this.voxelDepth,  density.get(index)*255));
-				/**
-				 * @todo multiplication with 255 not necessary, since we have no colors in input image
-				 */
-				  } else {
-			   	voxels.add(new VoxelImpl((int)x, (int)y, (int)z, this.voxelWidth, this.voxelHeight, this.voxelDepth,  0));
-				  }
-			   	/// note: density.get(index) in interval [0, 1] -> thus we multiply by 255 to have a color between 0 and 255
-			   	index++;
-			   }
-		 }
- 	}
-    }
+	public DensityImpl(HashMap<String, ArrayList<SWCCompartmentInformation>> stack, int voxelWidth, int voxelHeight, int voxelDepth, String choice) {
+		this.stack = stack;
+		this.voxelWidth = voxelWidth;
+		this.voxelHeight = voxelHeight;
+		this.voxelDepth = voxelDepth;
+		this.choice = choice;
+		compute();
+	}
 
-    @Override
-    @SuppressWarnings("ReturnOfCollectionOrArrayField")
-    public List<? extends VoxelSet> getVoxels() {
-        return voxels;
-    }
+	/**
+	 * @brief get the scaling factor
+	 * @return 
+	 */
+	public double getScalingFactor() {
+		return (backplane - frontplane) / (Collections.max(Arrays.asList(SWCUtility.getDimensions(stack).x, SWCUtility.getDimensions(stack).y, SWCUtility.getDimensions(stack).z)));
+	}
+
+	/**
+	 * Computes the average density for each voxel subset.
+	 */
+	private void compute() {
+		/// get the bounding box in physiological units, i. e. µm
+		Pair<Vector3f, Vector3f> bounding = SWCUtility.getBoundingBox(stack);
+		/// compute the density
+		HashMap<Integer, Float> density = SWCUtility.computeDensity(stack, voxelWidth, voxelHeight, voxelDepth, choice);
+		/**
+		 * @todo computeDensity must respect the voxel width depth and
+		 * height
+		 */
+
+		int index = 0;
+		for (float x = bounding.getSecond().x; x < bounding.getFirst().x; x += this.voxelWidth) {
+			for (float y = bounding.getSecond().y; y < bounding.getFirst().y; y += this.voxelHeight) {
+				for (float z = bounding.getSecond().z; z < bounding.getFirst().z; z += this.voxelDepth) {
+					if (density.containsKey(index)) {
+						voxels.add(new VoxelImpl((int) x, (int) y, (int) z, this.voxelWidth, this.voxelHeight, this.voxelDepth, density.get(index) * 255));
+						/**
+						 * @todo multiplication with 255
+						 * not necessary, since we have
+						 * no colors in input image
+						 */
+					} else {
+						voxels.add(new VoxelImpl((int) x, (int) y, (int) z, this.voxelWidth, this.voxelHeight, this.voxelDepth, 0));
+					}
+					/// note: density.get(index) in interval [0, 1] -> thus we multiply by 255 to have a color between 0 and 255
+					index++;
+				}
+			}
+		}
+	}
+
+	@Override
+	@SuppressWarnings("ReturnOfCollectionOrArrayField")
+	public List<? extends VoxelSet> getVoxels() {
+		return voxels;
+	}
 }
