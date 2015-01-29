6f660e71774aa2bc86d6c7e1cc1bcc2b9b75a838
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2015-01-29 15:11:48 +0100

    Adding missing interface files.

---
+++ src/main/java/edu/gcsc/vrl/swcdensityvis/types/DenseMatrixAbstractType.java
@@ -0,0 +1,9 @@
+package edu.gcsc.vrl.swcdensityvis.types;
+
+/**
+ *
+ * @author stephan
+ */
+interface DenseMatrixAbstractType {
+	
+}
---
+++ src/main/java/edu/gcsc/vrl/swcdensityvis/types/DenseMatrixBaseType.java
@@ -0,0 +1,13 @@
+/// package's name
+package edu.gcsc.vrl.swcdensityvis.types;
+
+/// imports
+import eu.mihosoft.vrl.reflection.TypeRepresentationBase;
+
+/**
+ *
+ * @author stephan
+ */
+public abstract class DenseMatrixBaseType extends TypeRepresentationBase implements DenseMatrixAbstractType {
+	private static final long serialVersionUID = 1L;
+}
